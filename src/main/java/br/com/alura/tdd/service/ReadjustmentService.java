5308144235b5463dc0d7960f6d4cfae585041c74
Marcelo Siqueira <marcelobolsoni@outlook.com>
2022-07-26 11:46:42 -0300

    Fix: refatorando a classe ReadjustmentService

--- src/main/java/br/com/alura/tdd/service/ReadjustmentService.java
+++ src/main/java/br/com/alura/tdd/service/ReadjustmentService.java
@@ -8,21 +8,11 @@ import br.com.alura.tdd.modelo.Performance;
 public class ReadjustmentService {
 
     public void makeReadjustment(Functionary functionary, Performance performance) {
-        if(performance == Performance.A_DESEJAR){
-            BigDecimal readjustment = functionary.getSalary().multiply(new BigDecimal("0.03"));
-
-            functionary.salaryReadjustment(readjustment);
-        }
-        else if(performance == Performance.BOM){
-            BigDecimal readjustment = functionary.getSalary().multiply(new BigDecimal("0.15"));
-            
-            functionary.salaryReadjustment(readjustment);
-        }
-        else if(performance == Performance.OTIMO){
-            BigDecimal readjustment = functionary.getSalary().multiply(new BigDecimal("0.20"));
-            
-            functionary.salaryReadjustment(readjustment);
-        }
+        //using strategy
+        BigDecimal percentage = performance.percentageReadjustment();
+        BigDecimal readjustment = functionary.getSalary().multiply(percentage);
+        
+        functionary.salaryReadjustment(readjustment);
     }
 
 }
